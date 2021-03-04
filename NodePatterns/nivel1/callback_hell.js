bd6dad9246761cdf46d44814f41fe5b5882b55f6
gambo94 <fedigambo94@gmail.com>
2021-03-04 15:04:27 +0100

    Pushing an updated version of the file into this branch

--- NodePatterns/nivel1/callback_hell.js
+++ NodePatterns/nivel1/callback_hell.js
@@ -3,9 +3,13 @@ const {
   readFile,
   writeFile
 } = require("fs/promises");
+
 const {
-  join
+  join,
+  basename,
+  resolve
 } = require("path");
+
 const inbox = join(__dirname, "inbox");
 const outbox = join(__dirname, "outbox");
 
@@ -18,43 +22,36 @@ const reverseText = str =>
 
 
 
-// this function receives the files of the array that it is generated
-// and returns a promise that reads the file and returns the data has a 'resolve'
-const loopAndRead =  (file) => {
-  // looping throught the array to create a number of promises equal to the number of files
-      return new Promise((resolve, reject) => {
-        let data = readFile(join(inbox, file), "utf8");
-        console.log('file: ' + file + ' read successfully');
-        resolve(data) ;
-      })
-}
-
-const promiseArray = [];
-let fileNames = [];
-
-// this function creates an array of promises which its number is equal
-// to the number of files
-const promiseCreator = (array) => {
-  for (let i = 0; i < array.length; i++){
-    // creating array of promises
-    promiseArray.push(loopAndRead(array[i]));
-
-    // pushing the filename into another array
-    fileNames.push(array[i]);
+const generatePromises = (array, inputFolder) => {
+  const proimsesArray = [];
+  for (file of array){
+      
+      // generates as many promises as the number of files
+      proimsesArray.push(readFile(join(inputFolder, file), 'utf-8'));
+      console.log(`The file ${file} has been read correctly`);
   }
-  // returns an array with promises (number equals to file(s))
-  return Promise.all(promiseArray)
+  return proimsesArray;
 }
 
 
-readdir(inbox)
-  .then((filesArray) =>  promiseCreator(filesArray))
-  .then((ArrayWithPromisedData) => {
-    for (let i=0; i< ArrayWithPromisedData.length; i++ ){
-      writeFile(join(outbox, fileNames[i]), reverseText(ArrayWithPromisedData[i]), "utf8");
-      console.log('file: ' + fileNames[i] + ' has been reversed successfully');
-    }
-    fileNames = [];
-    })
-  .catch(error => console.log(error))
+const reverseFileText = async (inputFolder, outputFolder) => {
+
+  // generates an array of files once the function has read the entire given folder
+  const arrayFiles = await readdir(inputFolder);
+  // generating an array of promises that return the data of the read files only when all the data has been read
+  const arrayOfPromises = await generatePromises(arrayFiles, inputFolder);
+
+
+  // iterating over the promises and solving them by writing the data collected before
+  for (let i = 0; i < arrayOfPromises.length; i++){
+      arrayOfPromises[i].then((data) => {
+          writeFile(join(outputFolder, arrayFiles[i]), reverseText(data));
+          console.log(`The file: ${arrayFiles[i]} has been reverse written correctly, check the folder: ${basename(resolve(outputFolder))}`);
+      })
+  }
+  return arrayFiles;
+}
+
+reverseFileText(inbox, outbox)
+.catch((err) => console.log(err));
 
